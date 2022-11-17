a49f126904a4568e1db2be74be14a2f53defe0ef
arturv <arturv@scribd.com>
2022-11-17 16:26:35 +0200

    upd

--- pod_check.sh
+++ pod_check.sh
@@ -3,12 +3,12 @@
 number=1
 while [[ $number -le 12 ]]
 do
-  echo $number
-  sleep 10
-  cmd=$(kubectl --kubeconfig $1 -n jenkins-stage get pods -l app=jenkins-stage -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}')
-  echo $cmd
-  if [[ $cmd = "True" ]]; then echo "Pod is ready"; break; fi
-  if [[ $number -eq 12 ]]; then echo "Pod awaiting timeout"; exit 1; fi
+  echo "${number}0 sec checking..."
+  sleep 1
+  cmd=$(kubectl --kubeconfig $1 -n jenkins get pods -l app=jenkins -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}')
+  echo "kubectl pod status output: $cmd"
+  if [[ $cmd = "True2" ]]; then echo "Pod is ready"; break; fi
+  if [[ $number -eq 4 ]]; then echo "Pod awaiting timeout"; exit 1; fi
   let number++
 done
 
