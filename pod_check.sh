9c62fb4662b81425f3df949ddea7014501f62e12
arturv <arturv@scribd.com>
2022-11-17 16:12:30 +0200

    upd

--- pod_check.sh
+++ pod_check.sh
@@ -6,7 +6,8 @@ do
   echo $number
   sleep 2
   cmd=$(kubectl --kubeconfig $1 -n jenkins get pods -l app=jenkins -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}')
-  if [[ $cmd = "True" ]]; then echo "Pod is ready"; break; fi
+  echo $cmd
+  if [[ $cmd = "True2" ]]; then echo "Pod is ready"; break; fi
   if [[ $number -eq 2 ]]; then echo "Pod awaiting timeout"; \
     kubectl --kubeconfig $1 -n jenkins get svc; exit 1; fi
   let number++
